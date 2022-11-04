b5f474fed451c2dc74b55163d2a74e6e2c9707a3
“dvh5561” <dvh5561@tamu.edu>
2022-11-03 21:51:39 -0500

    backend server queries

--- server/index.js
+++ server/index.js
@@ -29,17 +29,197 @@ app.get('/', (req, res) => {
 app.get('/items', (req, res) => {
     items = []
     pool
-     .query('SELECT * FROM items;')
-     .then(query_res => {
-         for (let i = 0; i < query_res.rowCount; i++){
-             items.push(query_res.rows[i]);
-         }
-         const data = {items: items};
-         console.log(items);
-         res.send(data);
-         return;
-     });
- });
+    .query('SELECT * FROM items;')
+    .then(query_res => {
+        for (let i = 0; i < query_res.rowCount; i++){
+            items.push(query_res.rows[i]);
+        }
+        const data = {items: items};
+        console.log(items);
+        res.send(data);
+        return;
+    });
+});
+
+//SERVER GUI//
+
+// Get orderid
+app.get('/orderid', (req, res) => {
+    items = []
+    pool
+    .query('SELECT order_id from orders ORDER BY order_id DESC LIMIT 1;')
+    .then(query_res => {
+        for (let i = 0; i < query_res.rowCount; i++){
+            items.push(query_res.rows[i]);
+        }
+        const data = {items: items};
+        console.log(items);
+        res.send(data);
+        return;
+    });
+});
+
+// Get item uuid
+app.get('/itemuuid', (req, res) => {
+    items = []
+    pool
+    .query("SELECT uuid from order_items ORDER BY uuid DESC LIMIT 1;")
+    .then(query_res => {
+        for (let i = 0; i < query_res.rowCount; i++){
+            items.push(query_res.rows[i]);
+        }
+        const data = {items: items};
+        console.log(items);
+        res.send(data);
+        return;
+    });
+});
+
+// Get topping uuid
+app.get('/toppinguuid', (req, res) => {
+    items = []
+    pool
+    .query("SELECT uuid from order_toppings ORDER BY uuid DESC LIMIT 1;")
+    .then(query_res => {
+        for (let i = 0; i < query_res.rowCount; i++){
+            items.push(query_res.rows[i]);
+        }
+        const data = {items: items};
+        console.log(items);
+        res.send(data);
+        return;
+    });
+});
+
+// Get item quantity
+app.get('/quantity/:id', (req, res) => {
+    items = []
+    pool
+    .query(`SELECT item_quantity from inventory WHERE item_id=${req.params.id}`.replace(/:/g, ""))
+    .then(query_res => {
+        for (let i = 0; i < query_res.rowCount; i++){
+            items.push(query_res.rows[i]);
+        }
+        const data = {items: items};
+        console.log(items);
+        res.send(data);
+        return;
+    });
+});
+
+// Get id from name
+app.get('/itemid/:name', (req, res) => {
+    items = []
+    pool
+    .query(`SELECT id FROM items WHERE name=${req.params.name}`.replace(/:/g, ""))
+    .then(query_res => {
+        for (let i = 0; i < query_res.rowCount; i++){
+            items.push(query_res.rows[i]);
+        }
+        const data = {items: items};
+        console.log(items);
+        res.send(data);
+        return;
+    });
+});
+
+// Insert order table
+app.get('/createorder/:orderID/:calories/:total/:tip_double/:total_after_tip/:after_tip/:date', (req, res) => {
+    items = []
+    pool
+    .query(`INSERT INTO orders VALUES (${req.params.orderID},${req.params.calories},${req.params.total},${req.params.tip_double},${req.params.total_after_tip},${req.params.after_tip});${req.params.date}`.replace(/:/g, ""))
+    .then(query_res => {
+        for (let i = 0; i < query_res.rowCount; i++){
+            items.push(query_res.rows[i]);
+        }
+        const data = {items: items};
+        console.log(items);
+        res.send(data);
+        return;
+    });
+});
+
+// Update inventory after order
+app.get('/updateinventoryserver/:itemid', (req, res) => {
+    items = []
+    pool
+    .query(`UPDATE inventory SET item_quantity=GREATEST(item_quantity-1,0) WHERE item_id=(${req.params.itemid});UPDATE inventory SET num_sold=num_sold+1 WHERE item_id=(${req.params.itemid});`.replace(/:/g, ""))
+    .then(query_res => {
+        for (let i = 0; i < query_res.rowCount; i++){
+            items.push(query_res.rows[i]);
+        }
+        const data = {items: items};
+        console.log(items);
+        res.send(data);
+        return;
+    });
+});
+
+// Insert into order items
+app.get('/insertitem/:uuid/:orderid/:subitem/:id/:name', (req, res) => {
+    items = []
+    pool
+    .query(`INSERT INTO order_items VALUES (${req.params.uuid},${req.params.orderid},${req.params.subitem},${req.params.id},${req.params.name});`.replace(/:/g, ""))
+    .then(query_res => {
+        for (let i = 0; i < query_res.rowCount; i++){
+            items.push(query_res.rows[i]);
+        }
+        const data = {items: items};
+        console.log(items);
+        res.send(data);
+        return;
+    });
+});
+
+// Insert into order toppings
+app.get('/inserttopping:uuid/:orderid/:subitem/:id/:name', (req, res) => {
+    items = []
+    pool
+    .query(`INSERT INTO order_toppings VALUES (${req.params.uuid},${req.params.orderid},${req.params.subitem},${req.params.id},${req.params.name});`.replace(/:/g, ""))
+    .then(query_res => {
+        for (let i = 0; i < query_res.rowCount; i++){
+            items.push(query_res.rows[i]);
+        }
+        const data = {items: items};
+        console.log(items);
+        res.send(data);
+        return;
+    });
+});
+
+
+// app.get(, (req, res) => {
+//     items = []
+//     pool
+//     .query(`${req.params.}`.replace(/:/g, ""))
+//     .then(query_res => {
+//         for (let i = 0; i < query_res.rowCount; i++){
+//             items.push(query_res.rows[i]);
+//         }
+//         const data = {items: items};
+//         console.log(items);
+//         res.send(data);
+//         return;
+//     });
+// });
+
+
+// app.get(, (req, res) => {
+    //     items = []
+//     pool
+//     .query()
+//     .then(query_res => {
+//         for (let i = 0; i < query_res.rowCount; i++){
+//             items.push(query_res.rows[i]);
+//         }
+//         const data = {items: items};
+//         console.log(items);
+//         res.send(data);
+//         return;
+//     });
+// });
+
+ 
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`); 

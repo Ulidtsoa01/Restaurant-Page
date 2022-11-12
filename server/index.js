1e0b8f56e66960bda8bae267580bf653159aa6b4
Liberato Aguilar <liberatoalanaguilar@gmail.com>
2022-11-12 13:06:15 -0600

    Manager report backend functions

--- server/index.js
+++ server/index.js
@@ -588,7 +588,7 @@ app.get('/getpairstogether', (req, res) => {
     res.header("Access-Control-Allow-Origin", "*");
     items = []
     pool
-    .query()
+    .query(`SELECT entree_name, topping_name, COUNT(*) AS count FROM (SELECT entree_name, topping_name from orders INNER JOIN order_items ON orders.order_id = order_items.order_id INNER JOIN order_toppings ON orders.order_id = order_toppings.order_id AND order_items.order_subitem = order_toppings.order_subitem) as big GROUP BY entree_name, topping_name ORDER BY count DESC;`)
     .then(query_res => {
         for (let i = 0; i < query_res.rowCount; i++){
             items.push(query_res.rows[i]);
@@ -600,6 +600,37 @@ app.get('/getpairstogether', (req, res) => {
     });
 });
 
+app.get('/latestitemid', (req, res) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    items = []
+    pool
+    .query(`SELECT item_id FROM inventory ORDER BY id DESC LIMIT 1;`)
+    .then(query_res => {
+        for (let i = 0; i < query_res.rowCount; i++){
+            items.push(query_res.rows[i]);
+        }
+        const data = {items: items};
+        console.log(items);
+        res.send(data);
+        return;
+    });
+});
+
+app.get('/latestid', (req, res) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    items = []
+    pool
+    .query(`SELECT id FROM inventory ORDER BY id DESC LIMIT 1;`)
+    .then(query_res => {
+        for (let i = 0; i < query_res.rowCount; i++){
+            items.push(query_res.rows[i]);
+        }
+        const data = {items: items};
+        console.log(items);
+        res.send(data);
+        return;
+    });
+});
 
 // app.get(, (req, res) => {
 //     res.header("Access-Control-Allow-Origin", "*");

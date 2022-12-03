0881b1dba2a24f2fbe120d264f661d1bd7dce9ff
Jun Lee <jun@Juns-MacBook-Pro.local>
2022-12-03 15:34:20 -0600

    login page changed

--- frontend/src/views/LoginView.vue
+++ frontend/src/views/LoginView.vue
@@ -1,60 +1,80 @@
 <template>
-    <v-container fluid fill-height>
-        <v-row align="center" justify="center" style="height: 35vh">
-            <v-col cols="5">
-                <v-card light height="35vh">
-                    <v-row align="center" align-content="center" justify="center" style="height: 35vh">
-                        <v-col cols="auto" style="margin-right: 20%">
-                            <img height="100" src="../views/images/pom.png">
+    <v-container>
+      <br><br><br><br>
+      <v-row align="center" justify="center">
+        <v-col cols="12" sm="10">
+          <v-card class="elevation-10 mt-10">
+            <v-window v-model="step">
+              <v-window-item :value="1">
+                <v-row>
+                  <v-col cols="12" md="6">
+                    <v-card-text class="mt-12">
+                      <br><br><br><br>
+                      <h1 class="text-center">Login in to Your Account</h1>
+                      <h5 class="text-center  grey--text ">Log in to your account so you can use Pom & Honey
+                        POS system
+                      </h5>
+                      <v-row align="center" justify="center">
+                        <v-col cols="12" sm="8">
+                          <br><br><br><br><br>
+                          <center>
+                            <div id="buttonDiv"></div>
+                          </center>
                         </v-col>
-                        <v-col cols="auto">
-                            <v-container>
-                                <v-row>
-                                    <v-col>
-                                        <h2 style="color: black">Login</h2>
-                                    </v-col>
-                                </v-row>
-                                <v-row>
-                                    <v-col>
-                                        <div id="buttonDiv"></div> 
-                                    </v-col>
-                                </v-row>
-                            </v-container>
-                        </v-col>
-                    </v-row>
-                </v-card>
-            </v-col>
-        </v-row>
+                      </v-row>
+                    </v-card-text>
+                  </v-col>
+                  <v-col cols="12" md="6" class="white rounded-bl-xl">
+                    <div style="  text-align: center; padding: 180px 0;">
+                      <v-card-text class="white--text">
+                        <img height="200" src="../views/images/pom.png"> </img>
+                      </v-card-text>
+                    </div>
+                  </v-col>
+                </v-row>
+              </v-window-item>
+            </v-window>
+          </v-card>
+        </v-col>
+      </v-row>
     </v-container>
-</template>
-
-<script>
-import { translateAll, langs } from '../js/backend.js'
+  </template>
+  
+  <script>
+  import { translateAll, langs } from '../js/backend.js'
   import { promptLogin } from '../js/login.js';
-
-export default {
-  name: "App",
-  components: {
-  },
-
-  methods: {
-    move() {
-      this.$router.push({
-        path: 'client',
-      })
+  
+  export default {
+    data: () => ({
+      step: 1
+    }),
+    props: {
+      source: String
     },
-      signedIn(e) {
-          this.$router.push("/server/"+e.credential);
+  
+    methods: {
+      move() {
+        this.$router.push({
+          path: 'client',
+        })
       },
-  },
+      signedIn(e) {
+        this.$router.push("/server/" + e.credential);
+      },
+    },
     async mounted() {
-        /*await translateAll("es");*/
-        await promptLogin(this.signedIn);
+      /*await translateAll("es");*/
+      await promptLogin(this.signedIn);
     }
-};
-</script>
-
-<style>
-
-</style>
-
+  
+  }
+  </script>
+  <style scoped>
+  .v-application .rounded-bl-xl {
+    border-bottom-left-radius: 300px !important;
+  }
+  
+  .v-application .rounded-br-xl {
+    border-bottom-right-radius: 300px !important;
+  }
+  </style>

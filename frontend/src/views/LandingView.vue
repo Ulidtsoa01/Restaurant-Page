2b768c5576d0a4b5b6aff8a74aa19ed501b8ac8a
MikeAlphaXray <huangxia000@tamu.edu>
2022-12-05 09:25:15 -0600

    Updated landingview from front end branch

--- frontend/src/views/LandingView.vue
+++ frontend/src/views/LandingView.vue
@@ -1,8 +1,69 @@
 <template>
   <div>
-      <h1>Welcome</h1>
+    <div v-bind:style="heroImage">
+      <v-container fluid>
+        <v-row justify="end">
+            <v-col cols="auto">
+                <v-btn color="orange" @click="$router.push('login')">Log In</v-btn>
+            </v-col>
+        </v-row>
+        <v-row justify="center">
+            <v-col cols="auto">
+                <router-link
+                  :key="$route.path"
+                  :to="{ path: '/' }"
+                  style="text-decoration: none; color: inherit"
+                >
+                  <v-img id="logo" src="./images/pom.png" width="250"></v-img>
+                </router-link>
+            </v-col>
+        </v-row>
+        <v-row justify="center" style="margin-top: 100px">
+            <v-col cols="auto">
+                <v-btn color="orange" large @click="$router.push('client')">Order Now</v-btn>
+            </v-col>
+
+
+            <!--ColorBlind Mode Button-->
+            <!--
+            <v-btn elevation="2" class="ma-2" color="green">Colorblind Mode</v-btn>
+            -->
+        </v-row>
+      </v-container>
+    </div>
   </div>
 </template>
+
+<script>
+  // import HelloWorld from '../components/HelloWorld'
+
+  export default {
+    name: 'LandingView',
+
+    components: {
+    },
+      computed: {
+        heroImage() {
+          return {
+            backgroundImage: `url${require('./images/background.jpg')}`
+          };
+        }
+      },
+  }
+  
+</script>
+
+<style>
+main{
+  background-image: linear-gradient(to bottom, rgba(0, 0, 0, 0.0), rgb(42, 53, 117)), url(./background.jpg);
+  background-size: cover;
+}
+
+#logo {
+  margin-top: 3em;
+}
+</style>
+
 
 <script>
 // import HelloWorld from '../components/HelloWorld'
